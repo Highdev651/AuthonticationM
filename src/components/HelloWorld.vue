0b4d8481251803cc43d4e765a52c2b964f77db31
Highdev <artsiomfreelance@gmail.com>
2024-03-07 09:04:44 -0800

    full push

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div id="HelloWorld">
+    <HeadNav msg="sdfessfs"/>
+
+  </div>
+</template>
+<script>
+  import HeadNav from './HeadNav.vue'
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      HeadNav
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
